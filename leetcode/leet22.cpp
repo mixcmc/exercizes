487edf69a990691adcd0419b88124e19d9af5fee
mix <mixcs@yandex.ru>
2023-09-01 18:46:08 +0300

    add ok solution for problem 22

--- leetcode/leet22.cpp
+++ leetcode/leet22.cpp
@@ -9,8 +9,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_set>
 using namespace std;
 
+/* -------------------------------------- */
 bool has_nested(const string& s)
 {
 	int p = 0;
@@ -22,6 +24,7 @@ bool has_nested(const string& s)
 	return false;
 }
 
+/* bad solution  */
 vector<string> sol(int n)
 {
 	vector<string> res;
@@ -47,6 +50,34 @@ vector<string> sol(int n)
 	}
 	return res;
 }
+/* -------------------------------------- */
+/* 1. total n left and n right parentheses
+ * 2. on each step can open left or close right if have open left
+ * */
+
+void generator(vector<string>& v, int l, int r, string s, int& call_cnt)
+{
+	call_cnt++;
+	if (l == 0 && r == 0) {
+		v.push_back(s);
+		return;
+	}
+
+	if (l > 0) generator(v, l - 1, r, s + '(', call_cnt);
+	/* if no unbalanced '(' => no ')' */
+	if (r > 0 && r > l) generator(v, l, r - 1, s + ')', call_cnt);
+}
+
+vector<string> sol1(int n)
+{
+	vector<string> res;
+	string s;
+	int call_cnt = 0;
+	generator(res, n, n, s, call_cnt);
+	cout << "call_cnt = " << call_cnt << endl;
+	return res;
+}
+
 
 void print(const vector<string>& v)
 {
@@ -56,12 +87,55 @@ void print(const vector<string>& v)
 	cout << "Total: " << v.size() << " strings\n";
 	cout << "--------------\n";
 }
+void print_diff(const vector<string>& v1, const vector<string>& v2)
+{
+	unordered_set<string> s1, s2;
+	for (auto s : v1) s1.insert(s);
+	for (auto s : v2) s2.insert(s);
+
+	/* bigger */
+	auto s3 = (s1.size() > s2.size() ? s1 : s2);
+	/* smaller  */
+	auto s4 = (s1.size() < s2.size() ? s1 : s2);
+
+	int cnt = 1;
+	for (auto s : s3) {
+		if (s4.count(s) == 0) {
+			cout << "missing" << cnt++ << ": " << s << endl;
+		}
+	}
+	cout << "Total " << cnt - 1 << " missing strings\n";
+	cout << "----------------\n";
+}
+
+void print_doubles(const vector<string>& v)
+{
+	unordered_set<string> ss;
+	int cnt = 0;
+	for (auto s : v) {
+		if (ss.count(s) > 0) {
+			cout << "dup: " << s << endl;
+			cnt++;
+		}
+		ss.insert(s);
+	}
+	cout << "Total " << cnt << " duplicates\n";
+	cout << "----------------\n";
+}
 
 int main()
 {
-	for (int i = 0; i < 8; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		cout << "n = " << i << endl;
-		print(sol(i));
+		//auto r0 = sol(i);
+		auto r1 = sol1(i);
+		//print(r0);
+		//print_doubles(r0);
+		//print(r1);
+		//print_doubles(r1);
+		//print_diff(r0, r1);
+		cout << "Total " << r1.size() << " strings\n";
+		cout << "----------------\n";
 	}
 	return 0;
 }
